141aab776b056073f120c41b1b8671d7fb379ba4
rictusempra52 <rictusempra52@gmail.com>
2024-12-25 01:19:12 +0900

    googleのアカウント画像が出るようになった

--- include/header.php
+++ include/header.php
@@ -12,4 +12,43 @@
     <!-- メニュー -->
     <div class="menu">
     </div>
+    <div class="usericon">
+        <!-- googleアイコン -->
+    </div>
+    <script type="module">
+        // firebase
+        import { initializeApp }
+            from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+        import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+        initializeApp(firebaseConfig);
+
+        // usericonに、認証しているgoogleアカウントのアイコンを表示する処理
+        const usericon = document.querySelector(".usericon");
+        const auth = getAuth();
+        // 認証状態をチェック
+        onAuthStateChanged(auth, (user) => {
+            // 認証している場合
+            if (user) {
+                const profileImage = document.createElement("img");
+                // imgを50x50にする
+                profileImage.width = 50;
+                profileImage.height = 50;
+                // imgにアイコンを表示
+                profileImage.src = user.photoURL;
+                profileImage.alt = user.displayName;
+                // iconを丸くする
+                usericon.style.borderRadius = "50%";
+                // imgが丸くならない場合、はみ出た部分を隠す
+                usericon.style.overflow = "hidden";
+
+                // usericonにimgを追加
+                usericon.appendChild(profileImage);
+            }
+            // 認証していない場合
+            else {
+                usericon.innerHTML = "";
+            }
+        });
+
+    </script>
 </header>
